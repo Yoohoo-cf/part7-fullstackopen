82ed253bfd6c3981f496aa837cbe5533e93ee872
Remy <wangrui777111@gmail.com>
2023-08-02 11:34:17 -0400

    done with part7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,19 +2,26 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const { userExtractor } = require('../utils/middleware')
 
-blogsRouter.get('/', async(request, response) => {
+blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
-        .find({}).populate('user', { username: 1, name: 1})
-    response.json(blogs)      
+        .find({}).populate('user', { username: 1, name: 1 })
+    response.json(blogs)
 })
 
-blogsRouter.post('/', userExtractor, async(request, response) => {
+blogsRouter.get('/:id', async (request, response) => {
+    const blog = await Blog
+        .findById(request.params.id).populate('user', { username: 1, name: 1 })
+
+    response.json(blog)
+})
+
+blogsRouter.post('/', userExtractor, async (request, response) => {
     const body = request.body
- 
+
     const user = request.user
 
     if (!(body.title || body.url)) {
-        response.status(400).json({ error: 'Title of url are required'})
+        response.status(400).json({ error: 'Title of url are required' })
     }
 
     const blog = new Blog({
@@ -24,34 +31,34 @@ blogsRouter.post('/', userExtractor, async(request, response) => {
         likes: body.likes || 0,
         user: user._id
     })
- 
+
     const result = await blog.save()
     user.blogs = user.blogs.concat(result._id)
     await user.save()
 
-    response.status(201).json(result)     
+    response.status(201).json(result)
 })
 
 
-blogsRouter.delete('/:id', userExtractor, async(request, response) => {
+blogsRouter.delete('/:id', userExtractor, async (request, response) => {
     const blog = await Blog.findById(request.params.id)
-   
+
     const user = request.user
 
-    if(blog.user.toString() === user.id.toString() ){
+    if (blog.user.toString() === user.id.toString()) {
         await Blog.findByIdAndRemove(request.params.id)
-        response.status(204).end() 
+        response.status(204).end()
     } else {
-        return response.status(403).json({ error: 'authorization required to delete'})
+        return response.status(403).json({ error: 'authorization required to delete' })
     }
 
 })
-  
 
-blogsRouter.put('/:id', async(request, response) => {
-    const { likes} = request.body
 
-    const updatedBlog= await Blog.findByIdAndUpdate(request.params.id, {likes}, { new: true})
+blogsRouter.put('/:id', async (request, response) => {
+    const { likes } = request.body
+
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, { likes }, { new: true })
     response.status(200).json(updatedBlog)
 })
 
