8d75869fe774e62a704bd96d5365380540cbd035
Remy <wangrui777111@gmail.com>
2023-08-02 11:13:39 -0400

    done for part7.1

---
+++ src/hooks/index.js
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+export const useField = () => {
+    const [value, setValue] = useState('')
+
+    const onChange = (e) => {
+        setValue(e.target.value)
+    }
+
+    const reset = () => {
+        setValue('')
+    }
+
+    return {
+        value,
+        onChange,
+        reset
+    }
+}
